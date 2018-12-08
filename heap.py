31bc8f3686e8e952a10612813e3b012ea8012090
sankle <santi.klein@hotmail.com>
2018-12-08 19:16:12 -0300

    Heap funcional

--- heap.py
+++ heap.py
@@ -1,64 +1,70 @@
-FACTOR = 1
+INDICE_DATO = 1
 
-class Heap_minimo:
-	def __init__(self):
+def comparar_pesos(peso1, peso2):
+	dato1 = peso1[INDICE_DATO]
+	dato2 = peso2[INDICE_DATO]
+	return (dato1-dato2)*(-1)
+
+class Heap:
+	def __init__(self, func_cmp):
 		self.lista = []
 		self.cantidad = 0
+		self.func_cmp = func_cmp
 
 	def __len__(self):
 		return self.cantidad
+	
+	def upheap(self, posicion):
+		if not posicion:
+			return
+		padre = (posicion-1)//2
+		if (self.func_cmp(self.lista[padre], self.lista[posicion])) < 0:
+			self.lista[padre], self.lista[posicion] = self.lista[posicion], self.lista[padre]
+			self.upheap(padre)
+
+	def downheap(self, cantidad, posicion):
+			if posicion >= cantidad:
+				return
+			maximo = posicion;
+			izq = 2 * posicion + 1;
+			der = 2 * posicion + 2;
+			if izq < cantidad and self.func_cmp(self.lista[izq], self.lista[maximo]) > 0:
+				maximo = izq
+
+			if der < cantidad and self.func_cmp(self.lista[der], self.lista[maximo]) > 0:
+				maximo = der
+
+			if maximo != posicion:
+				self.lista[posicion], self.lista[maximo] = self.lista[maximo], self.lista[posicion]
+				self.downheap(cantidad, maximo)
 
 	def encolar(self, dato):
 		self.lista.append(dato)
-		self.upheap(self.lista,self.cantidad)
-		self.cantidad +=1
+		self.upheap(self.cantidad)
+		self.cantidad += 1
 
-	def esta_vacia(self):
-		return self.cantidad ==0
+	def esta_vacio(self):
+		return self.cantidad == 0
 
 	def ver_minimo(self):
-		if(self.esta_vacia()):
+		if(heap_esta_vacia(self)):
 			return None
 		return self.lista[0]
 
 	def desencolar(self):
-		if self.esta_vacia():
-			raise Exception ('heap esta vacia')
-
-
+		if self.esta_vacio():
+			raise Exception ("El Heap esta vacio")
 		dato = self.lista[0]
-		self.cantidad -=1
+		self.cantidad -= 1
 		self.lista[0] = self.lista[self.cantidad]
-		self.downheap(self.lista, self.cantidad, 0)
+		self.lista.pop(self.cantidad)
+		self.downheap(self.cantidad, 0)
 		return dato
 
-	def heapify(self,arreglo, n):
-		for i in range (n/2, 1, -1):
-			self.downheap(arreglo, n, i-1)
+	
 
-	def upheap(self,arreglo, posicion):
-		if not posicion:
-			return
-		padre = int((posicion - 1)/2)
-		if(arreglo[padre] < arreglo[posicion]):
-			arreglo[padre], arreglo[posicion] = arreglo[posicion], arreglo[padre]
-			self.upheap(arreglo, padre)
-
-	def downheap(self,arreglo, cantidad, posicion):
-		if posicion >=cantidad:
-			return
-		minimo = posicion;
-		izq = 2 * posicion + 1;
-		der = 2 * posicion + 2;
-		if izq < cantidad and arreglo[izq] > arreglo[minimo]:
-			minimo = izq
-
-		if der < cantidad and arreglo[der]> arreglo[minimo]:
-			minimo = der
-
-		if minimo != posicion:
-			arreglo[posicion], arreglo[minimo] = arreglo[minimo], arreglo[posicion]
-			self.downheap(arreglo, cantidad, minimo)
+	
+	
 
 
 
